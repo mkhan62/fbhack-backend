81fd091954d5b04d30ade3db573a220853a6d9ea
mkhan62 <mkhan62@hawk.iit.edu>
2018-11-15 17:48:41 -0800

    Instantiate database object

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 """Backend Mangement for Activty Creator Chatbot."""
 from flask import Flask, request, jsonify
-import pyrebase
 from db import Firebase
 
 app = Flask(__name__)
@@ -8,13 +7,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    """Return all users"""
+    """Return all users."""
+    db = Firebase()
     result = db.read_all_users()
     return jsonify(result)
 
 
 @app.route('/add', methods=['POST'])
 def add():
+    """Add a user."""
+    db = Firebase()
     data = request.get_json()
     result = db.add_user(data)
     return jsonify(result)
@@ -22,10 +24,11 @@ def add():
 
 @app.route('/delete', methods=['POST'])
 def delete():
+    """Delete a user."""
+    db = Firebase()
     result = db.delete_entry("12234")
     return jsonify(result)
 
 
 if __name__ == '__main__':
-    db = Firebase()
     app.run()
